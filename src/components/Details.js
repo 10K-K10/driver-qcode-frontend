4994f3e661098d3193bff0a654ee1b9996bc825a
Ketan Rai <71873302+10K-K10@users.noreply.github.com>
2021-12-18 10:53:19 +0530

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -48,7 +48,7 @@ function Details() {
 								Name: <span>{details.name}</span>
 							</p>
 							<p>
-								DOB: <span>{details.dob.slice(0, 9)}</span>
+								DOB: <span>{details.dob.slice(0, 10)}</span>
 							</p>
 							<p>
 								Address: <span>{details.address}</span>
